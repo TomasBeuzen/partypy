0cfc4089d121da0f7ce77601b511bb32937ac145
Tomas Beuzen <tomas.beuzen@stat.ubc.ca>
2021-08-12 09:14:21 +1000

    refactor: add namespace imports to __init__.py

--- src/partypy/__init__.py
+++ src/partypy/__init__.py
@@ -1,5 +1,10 @@
-# importlib is included in the standard library for Python >= 3.8
-# here __version__ is obtained from installed version of package
+# read version from installed package
 from importlib.metadata import version
-
 __version__ = version(__name__)
+
+# populate package namespace
+from partypy.simulate import simulate_party
+from partypy.plotting import plot_simulation
+from partypy.datasets import load_party
+
+__all__ = ["simulate_party", "plot_simulation"]
