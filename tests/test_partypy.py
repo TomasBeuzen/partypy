5bbf1d8866ccc8871a18158329103ed575515a8e
Tomas Beuzen <tomas.beuzen@stat.ubc.ca>
2021-08-12 09:15:10 +1000

    test: add n_simulations unit test

--- tests/test_partypy.py
+++ tests/test_partypy.py
@@ -2,21 +2,23 @@ from partypy.simulate import simulate_party
 from partypy.plotting import plot_simulation
 import pandas as pd
 import altair as alt
+from pytest import raises  
 
 
 def test_simulate_party():
-    p_0 = [0, 0, 0]  # 3 guests with probability 0 of attending
-    p_1 = [1]        # 1 guest with probability 1 of attending
-    assert isinstance(simulate_party(p_0), pd.DataFrame)        # simulate_party outputs a dataframe
-    assert simulate_party(p_0, 10)["Total guests"].sum() == 0   # 0 probability, 10 simulations gives 0 sum
-    assert simulate_party(p_1, 10)["Total guests"].sum() == 10  # 1 probability, 10 simulations gives 10 sum
+    p_0 = [0, 0, 0]
+    p_1 = [1]
+    assert isinstance(simulate_party(p_0), pd.DataFrame)
+    assert simulate_party(p_0, 10)["Total guests"].sum() == 0
+    assert simulate_party(p_1, 10)["Total guests"].sum() == 10
+    with raises(ValueError):  # here is our new test
+        simulate_party([0], n_simulations=-5)
 
 
 def test_plot_simulation():
-    p_0 = [0, 0, 0]  # 3 guests with probability 0 of attending
+    p_0 = [0, 0, 0]
     results = simulate_party(p_0)
     plot = plot_simulation(results)
-    assert isinstance(plot, alt.Chart)  # plot_simulation outputs an alt.chart
-    assert plot.mark == "bar"           # chart type is "bar"
-    assert plot.data["Total guests"].sum() == 0  # 0 probability gives 0 sum
-    
+    assert isinstance(plot, alt.Chart)
+    assert plot.mark == "bar"
+    assert plot.data["Total guests"].sum() == 0
