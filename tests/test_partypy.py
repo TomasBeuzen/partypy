a7b367bd358266fcc7eb65289eb7eb2bd3bb1965
Tomas Beuzen <tomas.beuzen@stat.ubc.ca>
2021-08-12 09:15:23 +1000

    test: use data fixture in tests

--- tests/test_partypy.py
+++ tests/test_partypy.py
@@ -2,22 +2,27 @@ from partypy.simulate import simulate_party
 from partypy.plotting import plot_simulation
 import pandas as pd
 import altair as alt
-from pytest import raises  
+from pytest import raises, fixture
 
 
-def test_simulate_party():
-    p_0 = [0, 0, 0]
-    p_1 = [1]
-    assert isinstance(simulate_party(p_0), pd.DataFrame)
-    assert simulate_party(p_0, 10)["Total guests"].sum() == 0
-    assert simulate_party(p_1, 10)["Total guests"].sum() == 10
-    with raises(ValueError):  # here is our new test
+@fixture()
+def test_data():
+    return {
+        "p_0": [0, 0, 0],  # 3 guests with probability 0 of attending
+        "p_1": [1],        # 1 guests with probability 1 of attending
+    }
+
+
+def test_simulate_party(test_data):
+    assert isinstance(simulate_party(test_data["p_0"]), pd.DataFrame)
+    assert simulate_party(test_data["p_0"], 10)["Total guests"].sum() == 0
+    assert simulate_party(test_data["p_1"], 10)["Total guests"].sum() == 10
+    with raises(ValueError):
         simulate_party([0], n_simulations=-5)
 
 
-def test_plot_simulation():
-    p_0 = [0, 0, 0]
-    results = simulate_party(p_0)
+def test_plot_simulation(test_data):
+    results = simulate_party(test_data["p_0"])
     plot = plot_simulation(results)
     assert isinstance(plot, alt.Chart)
     assert plot.mark == "bar"
